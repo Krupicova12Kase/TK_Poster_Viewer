2af08031ff7d490ded5c74e587ddfc278bf606ec
Targex25 <luki123cz@gmail.com>
2026-01-04 17:35:13 +0100

    README Update and minor bug fixes
    Updated README
    Added License and credits
    Added Check to make sure there are always four .pptx files
    Fixed hardcoded section

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ Tento Python program vezme všechny čtyři části posteru na Tvořivou Kláves
 
 ### Použití programu
 
-1. Stáhněte soubor main.py a umístěte ho do složky mimo ostatní soubory (Mohlo by se stát, že by program použil jiné soubory místo posterů)
+1. Stáhněte soubor `main.py` a umístěte ho do složky mimo ostatní soubory (Mohlo by se stát, že by program použil jiné soubory místo posterů)
 2. Vložte všechny čtyři části posteru (hlavičku, levou část, pravou část a střední část) do stejné složky. Soubory nemusí být pojmenované podle příručky, program vezme vše co má koncovku .pptx
-3. Spusťte main.py. Program by měl začít převádět prezentace na PNG soubory, které následně uloží do složky output a nakonec spojí do jednoho.
+3. Spusťte `main.py`. Program by měl začít převádět prezentace na PNG soubory, které následně uloží do složky output a nakonec spojí do jednoho.
 4. Po několika sekundách by se měl ukázat celý poster
 
 
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+################################################################################
+# Report issues at https://github.com/Krupicova12Kase/TK_Poster_Viewer
+# Created by Krupicova12Kase, AKA Máťa or luki
+# MIT license 
+# Copyright (c) 2026 Krupicova12Kase
+################################################################################
+
+#Imports
 import win32com.client
 import os
 from PIL import Image
 
+names = [] 
+passed = False
 #Thank you stackoverflow https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 class bcolors:
     HEADER = '\033[95m'
@@ -35,57 +45,72 @@ def export_slide(pptx_path, output_folder,file):
     ppt_app.Quit()
     print(f"Exported to: {bcolors.OKBLUE}{output_path}{bcolors.ENDC}")
 
-
 directory = os.fsencode(os.path.dirname(os.path.abspath(__file__)))
-   
+
+x = 0
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".pptx"): 
-        name = filename[:filename.rfind(".")]
-        img = Image.new("RGB", (64,64),(255,255,255))
-        img.save("output/" + name + ".png", "PNG")
-        export_slide(filename,"output",name + ".png")
-        print(f"{bcolors.OKGREEN}Presentation converted succesfully!{bcolors.ENDC}")
+        x += 1
 
-
-#Image merge
-p1 = Image.open("output/ZIMČÍK_L_horni_2025.png").convert("RGBA")
-p2 = Image.open("output/ZIMČÍK_L_leva_2025.png").convert("RGBA")
-p3 = Image.open("output/ZIMČÍK_L_prava_2025.png").convert("RGBA")
-p4 = Image.open("output/ZIMČÍK_L_stred_2025.png").convert("RGBA")
-
-
-#Export everything
-posterimg = [p1,p2,p3,p4]
-
-#Calculate the width and height 
-h = p1.height + p4.height
-w = p2.width + p3.width + p4.width 
-print("")   
-print(f"Final Image Height: {h}")
-print(f"Final Image Width: {w}")
-print("") 
-#boxes
-#(horni sirka, horni vyska)
-b2 = (0, p1.height)
-b4 = (p2.width, p1.height,)
-b3 = (p2.width+p4.width, p1.height)
-b1 = (p2.width-int(round(p4.width/4)), 0)
-
-#Pasting
-fimg = Image.new("RGBA", (w,h),(255,255,255,0))
-
-fimg.paste(p2,b2)
-fimg.paste(p4,b4)
-fimg.paste(p3,b3)
-fimg.paste(p1,b1)
-
-#Displaying and saving
-fimg.save("output/" + "final_merged" + ".png", "PNG")
-save = fimg.show()
-savei = input("Save the image? (y/n) ").lower()
-if savei == "y" or savei == "yes":
-    fimg.save("output/" + "final_mergedx" + ".png", "PNG")
-    print(f"{bcolors.OKGREEN}Saved successfully!{bcolors.ENDC}")
+if x == 4:
+    print(f"{bcolors.OKGREEN}Found {4} .pptx files, attempting conversion{bcolors.ENDC}")
+    passed = True
+    print("")
+elif x > 4 or x < 4:
+    print(f"{bcolors.FAIL}Found {x} .pptx files, unable to convert, need exactly 4!{bcolors.ENDC}")
+    passed = False
+    input()
 else:
-    print("Not saving")
+    print(f"{bcolors.FAIL}Something strange happened during checking amount of .pptx files, make sure there are exactly four! (found {x})If yes, open an issue on GitHub{bcolors.ENDC}")
+    passed = False
+    input()
+    
+if passed:
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".pptx"): 
+            name = filename[:filename.rfind(".")]
+            img = Image.new("RGB", (64,64),(255,255,255))
+            img.save("output/" + name + ".png", "PNG")
+            export_slide(filename,"output",name + ".png")
+            names.append(str("output/"+name + ".png"))
+            print(f"{bcolors.OKGREEN}Presentation converted successfully!{bcolors.ENDC}")
+
+    #Open Images
+    p1 = Image.open(names[0]).convert("RGBA")
+    p2 = Image.open(names[1]).convert("RGBA")
+    p3 = Image.open(names[2]).convert("RGBA")
+    p4 = Image.open(names[3]).convert("RGBA")
+
+    #Calculate the width and height 
+    h = p1.height + p4.height
+    w = p2.width + p3.width + p4.width 
+    print("")   
+    print(f"Final Image Height: {h}")
+    print(f"Final Image Width: {w}")
+    print("") 
+    #boxes
+    #(horni sirka, horni vyska)
+    b2 = (0, p1.height)
+    b4 = (p2.width, p1.height,)
+    b3 = (p2.width+p4.width, p1.height)
+    b1 = (p2.width-int(round(p4.width/4)), 0)
+
+    #Pasting
+    fimg = Image.new("RGBA", (w,h),(255,255,255,0))
+
+    fimg.paste(p2,b2)
+    fimg.paste(p4,b4)
+    fimg.paste(p3,b3)
+    fimg.paste(p1,b1)
+
+    #Displaying and saving
+    fimg.save("output/" + "final_merged" + ".png", "PNG")
+    save = fimg.show()
+    savei = input("Save the image? (y/n) ").lower()
+    if savei == "y" or savei == "yes":
+        fimg.save("output/" + "final_merged" + ".png", "PNG")
+        print(f"{bcolors.OKGREEN}Saved successfully!{bcolors.ENDC}")
+    else:
+        print("Not saving")
